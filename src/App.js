bd190bd90878ef7e4d29962232d42330d300d3fc
MrSportsNerd <mrsportsnerd@gmail.com>
2017-12-22 20:31:34 -0500

    initialized project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import 'font-awesome/css/font-awesome.min.css';
+import 'bulma/css/bulma.css';
+
+/*eslint-enable*/
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default App;
