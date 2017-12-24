329a89abed83776b3cfb96d04106e377bb925f49
MrSportsNerd <mrsportsnerd@gmail.com>
2017-12-23 19:35:18 -0500

    some visuals

--- src/About.js
+++ src/About.js
@@ -14,7 +14,7 @@ class About extends Component {
 
   render() {
     return (
-      <section className="greeting-container">
+      <section className="hero greeting-container">
         <h1 className="greeting-title"> 
           Hi! I'm Phil. 
           <br></br> I'm a student double-majoring 
